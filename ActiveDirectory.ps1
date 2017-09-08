0b5742b14df113cf889c1afb43bbec965c8abdb6
John Kerry <jk185160@ncr.com>
2017-09-08 00:52:10 -0400

    fixing xStorage usage

--- ActiveDirectory.ps1
+++ ActiveDirectory.ps1
@@ -58,13 +58,13 @@ Configuration ActiveDirectory
     
     xWaitforDisk Disk2
     {
-      DiskNumber = 2
-      RetryIntervalSec =$RetryIntervalSec
-      RetryCount = $RetryCount
+      DiskId = 2
+      RetryIntervalSec = 20
+      RetryCount = 30
     }
-
+    
     xDisk ADDataDisk {
-      DiskNumber = 2
+      DiskId = 2
       DriveLetter = "F"
       DependsOn = "[xWaitForDisk]Disk2"
     }
